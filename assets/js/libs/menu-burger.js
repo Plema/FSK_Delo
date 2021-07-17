de3cdd558e2ccd4f7d877de25216967401af2b92
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-07-17 23:00:08 +0300

    menu-burgers.js

--- assets/js/libs/menu-burger.js
+++ assets/js/libs/menu-burger.js
@@ -3,4 +3,13 @@ $(function () {
     $('.header-burger,.header-menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
+  $('.arrow').click(function (event) {
+    $('.project-list').slideToggle('slow');
+    $('.arrow').toggleClass('show-list');
+    $('.arrow, .header-link_project').toggleClass('header-link_project-bold');
+  });
+
+  $('.wrapper').css({
+    'margin-left': $('.header-decktop').width(),
+  });
 });
