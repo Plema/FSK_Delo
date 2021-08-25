35f5d6ebf0ee2620a9a12fb7da2fec1be772b7c7
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-08-25 14:38:53 +0300

    sort
    js

--- assets/js/tenders.js
+++ assets/js/tenders.js
@@ -5,3 +5,31 @@ $(function () {
     });
   }
 });
+var table = $('table');
+
+$('#filter-status__up, #filter-status__down')
+  .wrapInner('<span title="отсортировать по статусу"/>')
+  .each(function () {
+    var th = $(this),
+      thIndex = th.index(),
+      inverse = false;
+
+    th.click(function () {
+      table
+        .find('td')
+        .filter(function () {
+          return $(this).index() === thIndex;
+        })
+        .sortElements(
+          function (a, b) {
+            return $.text([a]) > $.text([b]) ? (inverse ? -1 : 1) : inverse ? 1 : -1;
+          },
+          function () {
+            // parentNode is the element we want to move
+            return this.parentNode;
+          },
+        );
+
+      inverse = !inverse;
+    });
+  });
