65f30edaf1f7944b8a2debad39ab3ebefc21a1b4
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-11-15 13:32:22 +0200

    fix

--- assets/js/libs/menu-burger.js
+++ assets/js/libs/menu-burger.js
@@ -3,7 +3,7 @@ $(function () {
     $('.header-burger,.header-menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
-  $('.arrow').click(function (event) {
+  $('.header-list-item .arrow').click(function (event) {
     $('.project-list').slideToggle('slow');
     $('.arrow').toggleClass('show-list');
     $('.arrow, .header-link_project').toggleClass('header-link_project-bold');
